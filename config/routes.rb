e17a0cb7fe240c54a352cb7de95fd8503ae80064
tadp <june@tutormentor.org>
2013-09-04 06:38:22 -0700

    finished video model, seeding database, initial homepage, video show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
-<<<<<<< HEAD
   get '/home', to: 'videos#home'
   resources :categories
   resources :videos
-=======
->>>>>>> 65609e9e319ae9d78df44ed7f0e5caa0a322e47f
 end
