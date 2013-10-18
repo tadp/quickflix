9ee2d94f98fb35a6de9295ac7e4ae4a4377f7f43
tadp <june@tutormentor.org>
2013-10-18 15:19:41 -0700

    carrier changes

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
 		config.storage = :fog
   config.fog_credentials = {
     :provider               => 'AWS',                                   # required
-    :aws_access_key_id      => 'ENV['S3_KEY']',                         # required
-    :aws_secret_access_key  => 'ENV['S3_SECRET']',                      # required
+    :aws_access_key_id      => "ENV['S3_KEY']",                         # required
+    :aws_secret_access_key  => "ENV['S3_SECRET']",                      # required
   }
   config.fog_directory  = 'name_of_directory'                           # required
 	else
