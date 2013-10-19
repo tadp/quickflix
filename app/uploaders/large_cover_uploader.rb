bbdf313678a24644859ca6d042573d8816dfe53f
tadp <june@tutormentor.org>
2013-10-18 20:48:23 -0700

    s3 implementation

--- app/uploaders/large_cover_uploader.rb
+++ app/uploaders/large_cover_uploader.rb
@@ -4,13 +4,17 @@ class LargeCoverUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-
-  include CarrierWave::MiniMagick
-  process :resize_to_fill => [665, 375]
-  
+  if Rails.env.staging? || Rails.env.production?
+    include CarrierWave::MiniMagick
+    process :resize_to_fill => [665, 375]
+  end
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  if Rails.env.staging? || Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- app/uploaders/small_cover_uploader.rb
+++ app/uploaders/small_cover_uploader.rb
@@ -4,13 +4,18 @@ class SmallCoverUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-
-  include CarrierWave::MiniMagick
-  process :resize_to_fill => [166, 236]
-  
+  if Rails.env.staging? || Rails.env.production?
+    include CarrierWave::MiniMagick
+    process :resize_to_fill => [166, 236]
+  end
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+
+  if Rails.env.staging? || Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
